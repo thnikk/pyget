aac8180e68510964f11e72dd1da6adf1c347b31f
thnikk <thnikkshop@gmail.com>
2024-05-03 13:35:02 -0700

    Rewrite using subprocess

--- pyget.py
+++ pyget.py
@@ -1,125 +1,139 @@
 #!/usr/bin/python3 -u
 """
-Newer, more modular python script for fetching torrents from rss feeds.
+Description: Pyget v3
 Author: thnikk
 """
-import urllib.parse as up
-from xml.etree import ElementTree as et
-from datetime import datetime, timedelta
-import concurrent.futures
-import subprocess
-import argparse
-import json
-import sys
 import os
+import sys
+import json
+from subprocess import run, CalledProcessError
+from datetime import datetime, timedelta
+from xml.etree import ElementTree as et
+import concurrent.futures
 import requests
 
-# Initialize parser
-parser = argparse.ArgumentParser(
-    description='pyget.py: A script for adding torrents from rss feeds.')
-parser.add_argument('-i', action='store_true', help='Ignore blacklist')
-parser.add_argument('-d', action='store_true', help='Dry run')
-args = parser.parse_args()
 
-pool = concurrent.futures.ThreadPoolExecutor()
+class Transmission():
+    """ Transmission """
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.torrents = self.get_torrents()
 
-default_config = {
-    "client": {
-        "host": "10.0.0.29",
-        "port": "9091"
-    },
-    "feeds": [
-        {
-            "url": "https://nyaa.si/?page=rss",
-            "path": "~/Videos/Anime",
-            "age": 30,
-            "common": "subsplease 720p -batch",
-            "shows": {
-                "Sousou no Frieren": "Season 01",
-            }
-        }
-    ]
-}
+    def add(self, url, path) -> None:
+        """ Add torrents """
+        run([
+                "transmission-remote", f"{self.ip}:{self.port}", "-w",
+                f"{path}", "-a", f"{url}",
+            ], check=False, capture_output=True)
 
-
-def load_config(path, default):
-    """ Load or create json config with default """
-    # Set up config file
-    config_path = os.path.expanduser(path)
-    # Check if config exists
-    if os.path.exists(config_path):
-        # If it does, load it as config
-        print("Config found in", config_path)
-        with open(config_path, 'r', encoding='utf-8') as config_file:
-            return json.load(config_file)
-    else:
-        print("Config not found, creating in", config_path)
-        with open(config_path, "w", encoding='utf-8') as config_file:
-            json.dump(default, config_file, indent=4)
-        print("Default config created. Please edit "
-              f"{config_path} before running again.")
-        sys.exit(0)
+    def get_torrents(self) -> None:
+        """ Get torrents in transmission """
+        try:
+            return [
+                torrent.split("  ")[-1].strip() for torrent in run(
+                    ["transmission-remote", f"{self.ip}:{self.port}", '-l'],
+                    check=True, capture_output=True
+                ).stdout.decode('utf-8').splitlines()]
+        except CalledProcessError:
+            print("Couldn't connect to daemon, exiting.", file=sys.stderr)
+            sys.exit(1)
 
 
-def add_torrents(client, path, url, age, blacklist):
-    """ Get torrent files from rss feed """
-    xml = requests.get(url, timeout=3)
-    root = et.fromstring(xml.content)
+class Torrent():  # pylint: disable=too-few-public-methods
+    """ Torrent """
+    def __init__(self, url, title, date):
+        self.url = url
+        self.title = title
+        self.date = date
+
+    def old(self, age) -> bool:
+        """ Check if torrent is within age limit """
+        return (datetime.now() - self.date) < timedelta(days=age)
+
+
+def get_config(path):
+    """ Load config from path """
+    try:
+        with open(
+            os.path.expanduser(path), 'r', encoding='utf-8'
+        ) as file:
+            return json.loads(file.read())
+    except json.decoder.JSONDecodeError:
+        print(
+            "Couldn't decode json config, check for errors.", file=sys.stderr)
+        sys.exit(1)
+    except FileNotFoundError:
+        with open(
+            os.path.expanduser(path), 'w', encoding='utf-8'
+        ) as file:
+            file.write(json.dumps(
+                {"client": {"host": "localhost", "port": "9091"},
+                    "feeds": [{
+                            "url": "https://nyaa.si/?page=rss",
+                            "path": "~/Videos/Anime",
+                            "age": 30,
+                            "uploader": "subsplease",
+                            "common": "720p -batch",
+                            "shows": {"Sousou no Frieren": "Season 01"}
+                    }]}, indent=4))
+        print(
+            f"Default config created in {path}. "
+            "Edit before running again to add shows.", file=sys.stderr)
+        sys.exit(1)
+
+
+def parse_xml(string):
+    """ Parse xml from string into list """
+    root = et.fromstring(string)
+    output = []
     for tag in root.findall('./channel/item'):
-        # Get torrent URL
-        link = tag.find('link').text
-        # Get torrent title
-        title = tag.find('title').text
-        # Get date
-        date = datetime.strptime(
-            tag.find("pubDate").text, '%a, %d %b %Y %H:%M:%S %z'
-        ).replace(tzinfo=None)
-        # If it's younger than specified age
-        if (datetime.now() - date) < timedelta(days=age):
-            if title not in blacklist or args.i:
-                print(f"Downloading {title}")
-                if args.d:
-                    # Add it to transmission
-                    subprocess.run(
-                        ["transmission-remote",
-                            f"{client}",
-                            "-w", f"{path}",
-                            "-a", f"{link}",
-                         ],
-                        check=False,
-                        capture_output=True
-                    )
+        try:
+            link = tag.find('link').text
+            title = tag.find('title').text
+            date = datetime.strptime(
+                tag.find("pubDate").text, '%a, %d %b %Y %H:%M:%S %z'
+            ).replace(tzinfo=None)
+        except TypeError:
+            continue
+        output.append(Torrent(link, title, date))
+    return output
 
 
-config = load_config("~/.config/pyget.json", default_config)
-
-# Generate torrent list with list comprehension
-torrent_list = [torrent.split("  ")[-1].strip() for torrent in subprocess.run(
-    ["transmission-remote",
-     f"{config['client']['host']}:{config['client']['port']}", "-l"],
-    check=True, capture_output=True).stdout.decode('utf-8').splitlines()]
-
-# Iterate through feeds
-for feed in config['feeds']:
-    # Get base URL
-    URL = feed['url']
-    # Iterate through shows in feed
+def get_feed(feed, tr):
+    """ Get individual feed """
     for show, season in feed['shows'].items():
-        # Split url into parts with urllib
-        parts = up.urlparse(URL)
-        # Get query dictionary
-        query_dict = dict(up.parse_qsl(parts.query))
-        # Append query to querystring
-        query_dict['q'] = f"{show.lower()} {feed['common'].lower()}"
-        # Add querystring to URL
-        parts = parts._replace(query=up.urlencode(query_dict))
-        # Get torrents for show
-        pool.submit(
-            add_torrents,
-            f"{config['client']['host']}:{config['client']['port']}",
-            f"{os.path.expanduser(feed['path'])}/{show}/{season}",
-            up.urlunparse(parts),
-            feed['age'],
-            torrent_list
-        )
-pool.shutdown(wait=True)
+        qs = {"q": show.lower(), "u": ""}
+        for key, value in {"u": "uploader", "q": "common"}.items():
+            try:
+                qs[key] = " ".join([feed[value].lower(), qs[key]])
+            except KeyError:
+                pass
+        xml = requests.get(feed['url'], params=qs, timeout=3).content
+        path = "/".join([feed['path'], show, season])
+        torrents = parse_xml(xml)
+        for torrent in torrents:
+            if (
+                torrent.title not in tr.torrents
+                and torrent.old(feed['age'])
+            ):
+                print(f"Adding {torrent.title}")
+                tr.add(torrent.url, path)
+
+
+def main():
+    """ Main function """
+    config = get_config('~/.config/pyget.json')
+
+    tr = Transmission(config['client']['host'], config['client']['port'])
+
+    pool = concurrent.futures.ThreadPoolExecutor()
+
+    for feed in config['feeds']:
+        pool.submit(get_feed, feed, tr)
+
+    pool.shutdown(wait=True)
+
+
+if __name__ == "__main__":
+    main()
